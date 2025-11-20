81c5d27d77334329987efbafae816244852724d2
Rachit Garg <rachitgarg1208@gmail.com>
2025-11-20 18:37:51 +0530

    feat: Text Summarizer model into 5 points

---
+++ projects/01_text_summarizer/main.py
@@ -0,0 +1,31 @@
+# Persona Based Prompting
+from openai import OpenAI #type:ignore
+from dotenv import load_dotenv#type:ignore 
+import json
+import os
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("GEMINI_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/"
+)
+
+SYSTEM_PROMPT = """
+    you are an AI Assistant which is best at summarizing long text into 5 bullet points.
+    You always respond in a concise manner.
+    your main aim is to carefully the long text and extract only the most important information.
+    The output must be an ordered list of 5 bullet points.
+    """
+
+user_text = input("\nEnter the text you want to summarize:\n\n")
+
+response=client.chat.completions.create(
+    model="gemini-2.5-flash", 
+    messages=[
+        {"role":  "system", "content": SYSTEM_PROMPT},
+        {"role":  "user", "content": user_text},
+    ]
+)
+
+print(response.choices[0].message.content) 
